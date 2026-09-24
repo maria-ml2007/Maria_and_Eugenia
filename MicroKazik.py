208915f8a06936bb893663b54dddcf9e81b2c51e
Eugenia5gor <manna0net@gmail.com>
2026-09-24 11:59:44 +0700

    Add files via upload
    Draft

---
+++ MicroKazik.py
@@ -0,0 +1,19 @@
+#МИКРОКАЗИК
+import random #модуль для создания рандомных чисел
+
+start = input("Do you want to play the game? (Yes/No) ")
+if start == "No":
+    print("Goodbye!")
+else:
+
+a = random.randint(1,9)
+b = random.randint(1,9)
+c = random.randint(1,9)
+
+print(a, b, c)
+
+if a == b and b == c:
+    print("You win!!!")
+else: 
+    print("try again")
+
